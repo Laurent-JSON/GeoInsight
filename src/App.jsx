64619709a9045ad0d2f57e954cc639697e7c3c1b
Laurent-JSON <lawrencetibasima@gmail.com>
2026-04-05 12:28:10 +0200

    feat: implement smooth scroll to selected place on map marker click

--- src/App.jsx
+++ src/App.jsx
@@ -16,14 +16,16 @@ const App = () => {
   const [coordinates, setCoordinates] = useState({ lat: 52.5200, lng: 13.4050 });
   const [bounds, setBounds] = useState({});
   const [childClicked, setChildClicked] = useState(null);
-
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     // console.log({coordinates}, {bounds});
+    setIsLoading(true);
 
     getPlacesData(bounds?.sw, bounds?.ne)
       .then(data => {
         setPlaces(data);
+        setIsLoading(false);
         // console.log(data);
       });
 
@@ -41,7 +43,7 @@ const App = () => {
       >
         <Grid size={{ xs: 12, md: 4 }} sx={{ height: '100%' }}>
           <List
-            // isLoading={isLoading}
+            isLoading={isLoading}
             childClicked={childClicked}
             places={places}
           // type={type}
